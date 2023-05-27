4ef4aa37b5afecaca5ce332c67fdcd711c0c7cb1
henriquefl24 <henrique.f.laurito@unesp.br>
2023-05-27 11:36:45 -0300

    27/05

--- src/Util/download.py
+++ src/Util/download.py
@@ -2,10 +2,29 @@ def download(links: list):
     """
     This method downloads files from the provided links and saves them to the specified path.
     """
-    from os.path import join
+    from os.path import join, exists, expanduser
     from os import getcwd, makedirs
+    from getpass import getpass
     from concurrent.futures import ThreadPoolExecutor
+    from Util.create_dodsrc import create_dodsrc
+    from Util.create_netrc import create_netrc
     from Util.download_file_progress import download_file_with_progress
+    from Util.credential import test_credentials
+
+    home_dir = expanduser("~")
+    netrc_path = join(home_dir, ".netrc")
+    dodsrc_path = join(home_dir, ".dodsrc")
+
+    if not exists(netrc_path) and not exists(dodsrc_path):
+        create_dodsrc()
+        username = input("Username: ")
+        password = getpass("Password: ")
+        if not username or not password:
+            raise Exception("Please insert your EARTHDATA LOGIN authentication in order to continue")
+        elif not test_credentials(username, password):
+            raise Exception("Invalid EARTHDATA LOGIN credentials")
+        else:
+            create_netrc(username=username, password=password)
 
     # Check if the specified directory exists and create it if it doesn't
     path = join(getcwd(), "downloaded_data")
