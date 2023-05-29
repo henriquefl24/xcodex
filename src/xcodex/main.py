93b280d77accd4d09c39b880ae4f051b0eb3d781
henriquefl24 <henrique.f.laurito@unesp.br>
2023-05-29 14:23:26 -0300

    update xcodex_tutorial.ipynb

--- src/xcodex/main.py
+++ src/xcodex/main.py
@@ -173,3 +173,21 @@ def xco2_extract(path: list[str],
     dataframe.reset_index(inplace=True, drop=False)
 
     return dataframe
+
+
+def download_file(start: str, end: int) -> None:
+    """
+    This method will effectively download .nc4 files from NASA
+    Args:
+        start: start date (i.e.: "1st of January, 2015")
+        end: amount of days (i.e.: 365 days)
+
+    Returns: None
+    """
+    from Util.date import calendar_days
+    from Util.generate_links import generate_links
+    from Util.download import download
+
+    date_list = calendar_days(start, end)
+    links = generate_links(date_list)
+    download(links)
